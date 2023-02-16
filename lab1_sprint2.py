0103f7738c0f81d76937058b63487cbd5b77eec3
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-02-16 12:27:05 -0300

    Query para 100 repositórios com arquivo csv

--- lab1_sprint2.py
+++ lab1_sprint2.py
@@ -1,54 +1,85 @@
-import json
-from python_graphql_client import GraphqlClient
+from graphqlclient import GraphQLClient
 from datetime import datetime as date
+import json
+import csv
 
 url = "https://api.github.com/graphql"
-
-headers = {"Authorization": "Token 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"}
-
+token = "Token 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"
 today = date.utcnow()
-
-variables = {
-    "query": "stars :> 100",
-    "type": "REPOSITORY",
-    "fist": 20,
-    "after": None,
-}
+variables = {"after": None}
 
 query = """
-query ($query: String!, $type: SearchType!, $first: Int!, $after: String) {
-  search(query: $query, type: $type, first: $first, after: $after) {
-    pageInfo{
-      hasNextPage
-      endCursor
-    }
+query ($after: String) {
+  search(query: "stars:>100", type: REPOSITORY, first: 20, after: $after) {
+    pageInfo{ endCursor }
     nodes {
       ... on Repository {
         nameWithOwner
         createdAt
-        pullRequests(states: MERGED) {
-          totalCount
-        }
-        primaryLanguage {
-          name
-        }
+        pullRequests(first: 1,states: MERGED) { totalCount }
+        primaryLanguage { name }
         updatedAt
-        releases(orderBy: {field: CREATED_AT, direction: DESC}) {
-          totalCount
-        }
-        closedIssues: issues(filterBy: {states: CLOSED}) {
-          totalCount
-        }
-        totalIssues: issues{
-          totalCount
-        }
+        releases(first: 1, orderBy: {field: CREATED_AT, direction: ASC}) { totalCount }
+        closedIssues: issues(first: 1,states: CLOSED) { totalCount }
+        totalIssues: issues(first: 1){ totalCount }
       }
     }
   }
 }
 """
-client = GraphqlClient(endpoint = url, headers = headers)
+client = GraphQLClient(url)
+client.inject_token(token=token)
 
+with open("dadosRepositorios1.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow([
+        "repository",
+        "createdAt",
+        "age",
+        "pullRequests",
+        "releases",
+        "updatedAt",
+        "daysSinceUpdate",
+        "primaryLanguage",
+        "closedIssues",
+        "totalIssues",
+        "issuesRatio",
+    ])
 
-response = client.execute(query=query, variables=variables)
-print(response)
+    for i in range(5):
+        data = json.loads(client.execute(query=query, variables=variables))
+        results = data["data"]["search"]
+        end_cursor = results["pageInfo"]["endCursor"]
+
+        variables["after"] = end_cursor
+        repositories = results["nodes"]
+
+        for repo in repositories:
+            name = repo["nameWithOwner"]
+            created_at = date.strptime(repo["createdAt"], "%Y-%m-%dT%H:%M:%SZ")
+            age = (today - created_at).days
+            pull_requests = repo["pullRequests"]["totalCount"]
+            language = repo["primaryLanguage"]["name"] if repo[
+                "primaryLanguage"] is not None else "none"
+            updated_at = date.strptime(repo["updatedAt"], "%Y-%m-%dT%H:%M:%SZ")
+            days_since_update = today - updated_at
+            releases = repo["releases"]["totalCount"]
+            closed_issues = repo["closedIssues"]["totalCount"]
+            total_issues = repo["totalIssues"]["totalCount"]
+            issues_ratio = closed_issues / total_issues if total_issues > 0 else 0
+
+            writer.writerow([
+                name,
+                created_at,
+                age,
+                pull_requests,
+                releases,
+                updated_at,
+                days_since_update,
+                language,
+                closed_issues,
+                total_issues,
+                issues_ratio,
+            ])
+
+print("fim")
